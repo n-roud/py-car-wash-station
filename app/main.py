e36e96d3d246b65c589ec53a4f9a0a4286725502
Anastasiia Khudyakova <sun14water16@gmail.com>
2024-10-21 21:53:27 +0300

    update version passed test passed flake8.01

--- app/main.py
+++ app/main.py
@@ -26,8 +26,8 @@ class CarWashStation:
         return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        return ((car.comfort_class * (self.clean_power - car.clean_mark)) *
-                (self.average_rating / self.distance_from_city_center))
+        return ((car.comfort_class * (self.clean_power - car.clean_mark))
+                * (self.average_rating / self.distance_from_city_center))
 
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
